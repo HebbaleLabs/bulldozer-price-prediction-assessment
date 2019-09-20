b1499b1ece83f20539c19041d277b4e812aeb6c3
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2019-09-20 11:14:00 +0530

    Added code for run phase detection.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import os
 import train
 import test
 
 
-def main():
+RUN_PHASE = os.getenv('RUN_PHASE', 'all')
+
+if RUN_PHASE in ['all', 'training']:
     print('Running training phase.')
     train.main()
+
+if RUN_PHASE in ['all', 'testing']:
     print('Running testing phase.')
     test.main()
-
-
-if __name__ == '__main__':
-    main()
